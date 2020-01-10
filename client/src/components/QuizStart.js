067c9d632a8bf47ff4d322f99c1fb9de061733d9
Lee Damien <notesong.git@outlook.com>
2020-01-10 18:18:32 -0500

    Footer fix and logo size adjustment

--- client/src/components/QuizStart.js
+++ client/src/components/QuizStart.js
@@ -11,7 +11,7 @@ const Image = styled.div`
     height: 200px;
     margin: 1rem 0;
     @media only screen and (max-width: 600px) {
-        height: 200px;
+        height: 100px;
     }
 `;
 
@@ -23,7 +23,7 @@ const QuizStart = () => {
             <header>
                 <Image />
                 <h3>Guess which of these Celebs are Dead or Alive!</h3>
-                <h3>You have 30 seconds.</h3>
+                <h4>You have 30 seconds.</h4>
                 {isNameHere === "" ? null : <h3> Let's Begin, {isNameHere}!</h3>}
             </header>
             <div className="begin_button">
