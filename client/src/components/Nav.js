c37efbc6b6d788d5f17a7bb7e0fd58428aa1b12d
Lee Damien <notesong.git@outlook.com>
2020-01-09 23:30:33 -0500

    Scoreboard styling

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 const Nav = () => {
     return (
       <nav id="nav">
+        <Link to='/'>Home</Link>
         <Link to='/Quiz'>Quiz</Link>
         <Link to='/Scoreboard'>Scoreboard</Link>
         <Link to='/login'><span className="button small">Login</span></Link>
